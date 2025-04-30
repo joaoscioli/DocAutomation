db9f9523989355caa74b572f504c23c57d9ee5aa
joaoscioli <joaoscioli@outlook.com>
2025-04-30 19:31:06 -0300

    feat(add package) add new package MVC architecture

---
+++ src/main/java/reportGenerator/Report.java
@@ -0,0 +1,20 @@
+package reportGenerator;
+
+import java.util.Date;
+
+public class Report {
+    public String nameFile;
+    public String name;
+    public String cpf;
+    public String endereco;
+    public String refConsulta;
+    public Date dataSolicitacao;
+    public String emailSolicitacao;
+    public String nomeSolicitacao;
+    public String justificativa;
+}
+
+// Constructor
+
+
+// Getters and Setters
---
+++ src/main/java/reportGenerator/ReportRepository.java
@@ -0,0 +1,4 @@
+package reportGenerator;
+
+public interface ReportRepository {
+}
